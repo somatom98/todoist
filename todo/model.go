801d24c577a6ba5c29502ffc27e93a4baff642bd
Tommaso Lauria <tommasolauria@Tommasos-MacBook-Pro.local>
2025-02-01 13:11:08 +0100

    chore: move model

---
+++ todo/model.go
@@ -0,0 +1,35 @@
+package todo
+
+import (
+	"github.com/charmbracelet/bubbles/list"
+)
+
+type Todo struct {
+	title       string
+	description string
+}
+
+var _ list.Item = Todo{}
+
+func New(title, description string) Todo {
+	return Todo{
+		title:       title,
+		description: description,
+	}
+}
+
+func (t Todo) String() string {
+	return t.title
+}
+
+func (t Todo) FilterValue() string {
+	return t.title
+}
+
+func (t Todo) Title() string {
+	return t.title
+}
+
+func (t Todo) Description() string {
+	return t.description
+}
--- todo/types.go
+++ todo/types.go
@@ -1,40 +1,6 @@
 package todo
 
-import (
-	"context"
-
-	"github.com/charmbracelet/bubbles/list"
-)
-
-type Todo struct {
-	title       string
-	description string
-}
-
-var _ list.Item = Todo{}
-
-func New(title, description string) Todo {
-	return Todo{
-		title:       title,
-		description: description,
-	}
-}
-
-func (t Todo) String() string {
-	return t.title
-}
-
-func (t Todo) FilterValue() string {
-	return t.title
-}
-
-func (t Todo) Title() string {
-	return t.title
-}
-
-func (t Todo) Description() string {
-	return t.description
-}
+import "context"
 
 type TodoRepo interface {
 	GetAll(ctx context.Context) ([]Todo, error)
